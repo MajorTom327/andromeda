3907d0a9b4ef58253de41e820cdccb9c6c11095a
vthomas <valentin.thomas@fasst.io>
2021-08-06 17:26:44 +0200

    Review task view

--- imports/ui/components/ListTask/TaskView.tsx
+++ imports/ui/components/ListTask/TaskView.tsx
@@ -31,37 +31,40 @@ const TaskView: React.FC<Props> = ({ task }) => {
   }, ref);
 
   return (
-    <div className="flex" ref={ref}>
-      <div className="flex-grow p-4 hover:shadow-lg bg-gray-500" {...longPress}>
-        <div className="flex justify-between">
-          <h4 className="text-lg">
-            {task.label}
-          </h4>
-          {ready ? <h5 className="p-2 px-4 rounded-full bg-gray-600">{project?.name}</h5> : (<div>Chargement en cours...</div>)}
+    <div className="flex drawer drawer-end" ref={ref}>
+      <div className="flex-grow card bordered bg-neutral drawer-content" {...longPress}>
+        <div className="card-body">
+          <div className="card-title flex justify-between">
+            <h4 className="text-lg">
+              {task.label}
+            </h4>
+            {ready ? <h5 className="badge badge-outline">{project?.name}</h5> : (<div>Chargement en cours...</div>)}
+
+          </div>
+
+          <p className="text-accent">
+            {task.detail}
+          </p>
 
         </div>
-
-        <p className="text-gray-400">
-          {task.detail}
-        </p>
       </div>
       {isOptionEnabled && (
-        <>
-          <div className="bg-gray-800 w-24 text-2xl" onClick={() => setIsOptionEnabled(false)}>
-            <div className="flex h-full flex-col justify-center">
-              <div className="flex justify-center">
-                <FontAwesomeIcon icon={faTimes} />
-              </div>
-            </div>
-          </div>
-          <div className="bg-red-500 w-24 text-2xl" onClick={() => Meteor.call('tasks.remove', task._id)}>
-            <div className="flex h-full flex-col justify-center">
-              <div className="flex justify-center">
+        <div className="drawer-side">
+          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            <li>
+              <a className="flex gap-4 text-error" onClick={() => Meteor.call('tasks.remove', task._id)} >
                 <FontAwesomeIcon icon={faTrash} />
-              </div>
-            </div>
-          </div>
-        </>
+                Supprimer
+              </a>
+            </li>
+            <li>
+              <a className="flex gap-4" onClick={() => setIsOptionEnabled(false)} >
+                <FontAwesomeIcon icon={faTimes} />
+                Annuler
+              </a>
+            </li>
+          </ul>
+        </div>
       )}
     </div>
   );
