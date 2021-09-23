8b1160e3848a487eb6e5b5280d1843ea312e5703
Frédéric KOLLER <43694853+fredckl@users.noreply.github.com>
2021-09-23 09:28:40 +0200

    Added listAction

--- imports/ui/components/ListActions.tsx
+++ imports/ui/components/ListActions.tsx
@@ -1,29 +1,55 @@
-import React from 'react'
+import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useClickAway } from 'ahooks';
+import React, { ReactNode, useRef, useState } from 'react';
+import useLongPress from '/imports/hooks/useLongPress';
 
 interface Props {
-  onCancel: () => void
   onRemove: () => void
+  children: ReactNode
 }
 
-const ListActions = ({onCancel, onRemove}: Props) => {
+const ListActions = ({ onRemove, children}: Props) => {
+  const [isOptionEnabled, setIsOptionEnabled] = useState<boolean>(false);
+
+  const ref = useRef<HTMLDivElement>();
+
+  const longPress = useLongPress(
+    () => {
+      setIsOptionEnabled(true);
+    },
+    () => { }, {
+    shouldPreventDefault: true,
+    delay: 500,
+  })
+
+  useClickAway(() => {
+    setIsOptionEnabled(false);
+  }, ref);
+  const handleCancel = () => setIsOptionEnabled(false);
   return (
-    <div className="drawer-side">
-      <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-        <li>
-          <a className="flex gap-4 text-error" onClick={onRemove} >
-            <FontAwesomeIcon icon={faTrash} />
-            Supprimer
-          </a>
-        </li>
-        <li>
-          <a className="flex gap-4" onClick={onCancel} >
-            <FontAwesomeIcon icon={faTimes} />
-            Annuler
-          </a>
-        </li>
-      </ul>
+    <div className="base-200 drawer drawer-end" ref={ref}>
+      <input id="drawer-actions" type="checkbox" className="drawer-toggle" checked={isOptionEnabled} onChange={() => {}}/>
+      <div className="flex flex-col card bordered bg-neutral drawer-content"  {...longPress}>
+        {children}
+      </div>
+      <div className={'drawer-side'}>
+        <label htmlFor="drawer-actions" className="drawer-overlay"></label>
+        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+          <li>
+            <a className="flex gap-4 text-error" onClick={onRemove} >
+              <FontAwesomeIcon icon={faTrash} />
+              Supprimer
+            </a>
+          </li>
+          <li>
+            <a className="flex gap-4" onClick={handleCancel} >
+              <FontAwesomeIcon icon={faTimes} />
+              Annuler
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
--- imports/ui/components/Task.tsx
+++ imports/ui/components/Task.tsx
@@ -1,38 +1,19 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
+import ITask from '../../api/types/Task';
 import { formatDateFR } from '../../helpers/date';
-import useLongPress from '/imports/hooks/useLongPress';
-import { useClickAway } from 'ahooks';
 import ListActions from './ListActions';
 import useTasks from '/imports/hooks/useTasks';
-import ITask from '../../api/types/Task';
 interface Props {
   task: ITask
 }
 
 const Task = ({task}: Props) => {
-  const [isOptionEnabled, setIsOptionEnabled] = useState<boolean>(false);
   const {handleRemoveTask} = useTasks();
-  const ref = useRef<HTMLDivElement>();
-
-  const longPress = useLongPress(
-    () => {
-      setIsOptionEnabled(true);
-    },
-    () => { }, {
-    shouldPreventDefault: true,
-    delay: 500,
-  })
-
-  useClickAway(() => {
-    setIsOptionEnabled(false);
-  }, ref);
 
   const handleRemove = () => handleRemoveTask(task._id)
-  const handleCancel = () => setIsOptionEnabled(false);
 
   return (
-    <div className="flex drawer drawer-end" ref={ref}>
-      <div className="flex-grow card bordered bg-neutral drawer-content"  {...longPress}>
+      <ListActions onRemove={handleRemove}>
         <div className="card-body">
           <div className="card-title flex flex-col sm:flex-row sm:justify-between gap-2">
             <h4 className="text-lg">
@@ -43,12 +24,8 @@ const Task = ({task}: Props) => {
           </div>
 
           <div className="text-accent" dangerouslySetInnerHTML={{__html: task.detail.replace(/\n/g, '<br />')}}></div>
-
         </div>
-
-      </div>
-      {isOptionEnabled && (<ListActions onRemove={handleRemove} onCancel={handleCancel}/>)}
-    </div>
+    </ListActions>
   )
 }
 
