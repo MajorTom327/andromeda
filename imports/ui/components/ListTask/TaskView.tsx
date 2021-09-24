635201967011a6d09928c9f111d8ab82918145b5
MajorTom327 <me@valentin-thomas.com>
2021-09-25 01:25:16 +0200

    Add markdown content view (#7)

--- imports/ui/components/ListTask/TaskView.tsx
+++ imports/ui/components/ListTask/TaskView.tsx
@@ -6,13 +6,14 @@ import ITask from '/imports/api/types/Task';
 import useLongPress from '/imports/hooks/useLongPress';
 import useProject from '/imports/hooks/useProject';
 import Loading from '../Loading';
+import ReactMarkdown from 'react-markdown';
 interface Props {
   task: ITask
 }
 
 const TaskView: React.FC<Props> = ({ task }) => {
   const [ready, project] = useProject(task.project);
-  const {handleRemoveTask} = useTasks();
+  const { handleRemoveTask } = useTasks();
 
   const [isOptionEnabled, setIsOptionEnabled] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement>();
@@ -32,19 +33,23 @@ const TaskView: React.FC<Props> = ({ task }) => {
 
   const handleRemove = () => handleRemoveTask(task._id)
 
-  if (!ready) return <Loading/>
+  if (!ready) return <Loading />
 
   return (
     <ListActions onRemove={handleRemove}>
       <div className="card-body">
-          <div className="card-title flex flex-col sm:flex-row sm:justify-between gap-2">
-            <h4 className="text-lg">
-              {task.label}
-            </h4>
-            {ready ? <h5 className="badge badge-outline">{project?.name}</h5> : (<div>Chargement en cours...</div>)}
-          </div>
-          <div className="text-accent" dangerouslySetInnerHTML={{__html: task.detail.replace(/\n/g, '<br />')}}></div>
+        <div className="card-title flex flex-col sm:flex-row sm:justify-between gap-2">
+          <h4 className="text-lg">
+            {task.label}
+          </h4>
+          {ready ? <h5 className="badge badge-outline">{project?.name}</h5> : (<div>Chargement en cours...</div>)}
         </div>
+        <div className="text-accent markdown">
+          <ReactMarkdown>
+            {task.detail}
+          </ReactMarkdown>
+        </div>
+      </div>
     </ListActions>
   );
 }
