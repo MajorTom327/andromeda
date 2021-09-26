e8a276f2d94674d6995bf96879c5f84423487c26
Frédéric KOLLER <43694853+fredckl@users.noreply.github.com>
2021-09-26 11:26:33 +0200

    add search tasks (#3)
    * add search tasks
    
    * Fix daily to use cursor
    
    * Set regex as case insentitive instead of lowercase all
    
    Co-authored-by: MajorTom327 <me@valentin-thomas.com>

--- imports/hooks/useAllTasks.ts
+++ imports/hooks/useAllTasks.ts
@@ -2,15 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import Tasks from '../api/collections/Tasks';
 import ITask from '../api/types/Task';
+import { Mongo } from 'meteor/mongo';
 
 type Response = [
   boolean,
-  ITask[]
+  Mongo.Cursor<ITask>
 ]
 
 export const useAllTasks = (task: Partial<ITask>): Response => useTracker(() => {
   const ready = Meteor.subscribe('tasks.all', task).ready();
-  const tasks = Tasks.find(task).fetch()
+  const tasks = Tasks.find(task)
 
   return [ready, tasks];
 });
